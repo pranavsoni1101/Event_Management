c192f347e35594232b72ef550dc78968fab54fe6
pranavsoni1101 <pranavsoni1101@gmail.com>
2021-05-06 18:21:26 +0530

    Rearranging files, introducing ejs, adding some new routes

--- index.js
+++ index.js
@@ -1,20 +1,33 @@
 const express = require("express");
 const app     = express();
 
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/public"));
+
 // Setting the Routes
 // '/' = Root route
 app.get('/', (req, res) => {
-    res.send("Home page");
+    res.render("home");
 });
 
 // '/login' = login form
 app.get('/login', (req, res) => {
-    res.send("Login page");
+    res.render("login");
 });
 
 // '/register' = register form
 app.get('/register', (req, res) => {
-    res.send("Register page");
+    res.render("register");
+});
+
+//Event Registration form
+app.get('/eventForm', (req, res) => {
+    res.render("eventForm");
+});
+
+//Participant Registration form
+app.get('/participantForm', (req, res) => {
+    res.render("participantForm");
 });
 
 // Fallback Route
@@ -22,9 +35,6 @@ app.get('*', (req, res) => {
     res.send("OOPS WRONG PAGE!");
 });
 
-// app.use(()=> {
-//     console.log("We got a request!!");
-// });
 
 app.listen(3000, ()=> {
     console.log("Listening on port 3000");
